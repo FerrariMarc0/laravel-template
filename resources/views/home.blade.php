d89a3217e17e86ce98ca379047cfe30a416aa146
FerrariMarc0 <marco1010@live.it>
2023-05-10 15:47:44 +0200

    temp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,5 @@
 @endsection
 
 @section('main')
-<div class="dropdown mt-5">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Dropdown button
-    </button>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Action</a></li>
-      <li><a class="dropdown-item" href="#">Another action</a></li>
-      <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
-  </div>
+<h1 class="text-center text-primary mt-5">Welcome</h1>
 @endsection
